f068efe6e89337797f447b4636546a60ff49fa06
Shahd <112779266+4ahd3ssam@users.noreply.github.com>
2025-03-22 21:15:40 +0200

    Update Gen_20221053_20221084_20221146_S3,4_Ahmed Ali.pl

--- Gen_20221053_20221084_20221146_S3,4_Ahmed Ali.pl
+++ Gen_20221053_20221084_20221146_S3,4_Ahmed Ali.pl
@@ -91,14 +91,13 @@ num_matches_of_team(Team, Count) :-
 % 2- find the max from this list
 % 3- return the max
 
-
 % Collect all scores
-get_all_scores(List) :- find_all_scores([], List). % all scores will be returned in List after append them to the empty one
+get_all_scores(List) :- find_all_scores([], List), !. % cut to prevent backtracking
 find_all_scores(Acc, List) :- % Acc = []
     goals(Player, Score), % for each goals fact in the file
-    \+ member(Score, Acc), % if the score is not memeber in Acc -unique list-
-    find_all_scores([Score | Acc], List). % recursive call with new Acc afetr append the current Score to it
-find_all_scores(List, List). % base case to stop after finish all goals facts
+    \+ member(Score, Acc), % if the score is not member in Acc -unique list-
+    find_all_scores([Score | Acc], List). % recursive call with new Acc after appending the current Score to it
+find_all_scores(List, List). % base case to stop after finishing all goals facts
 
 % Find the maximum
 calculate_max_score([X], X). % base case is when only one element then it's the max
@@ -110,40 +109,45 @@ calculate_max_score([H|T], Max) :-
 top_scorer(Player) :-
     get_all_scores(GoalsList),
     calculate_max_score(GoalsList, MaxGoals),
-    goals(Player, MaxGoals). % return the Player
-    
+    goals(Player, MaxGoals), !. % cut to return only one answer
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% task 7 %%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
-% Collect all positions for a specific team
-get_all_positions(Team, Positions) :-
-    find_all_positions(Team, Positions, []).
-find_all_positions(Team, Positions, Acc) :-
-    player(_, Team, Position),
-    \+ member(Position, Acc),
-    find_all_positions(Team, Positions, [Position|Acc]).
-find_all_positions(_, Positions, Positions).
+% To Find The Most Common Position in a Specific Team:
+% 1- get all positions for the team
+% 2- calculate the frequency number for each position
+% 3- return the position for the max frequency number
 
-% Count occurrences of each unique element in a list
-get_all_frequencies([], [], []).
-get_all_frequencies([H|T], Unique, Counts) :-
+% Get all positions for a the team
+get_all_positions(Team, Positions) :- % return all positions in Positions list
+    find_all_positions(Team, Positions, []).
+find_all_positions(Team, Positions, Acc) :- % Acc = []
+    player(_, Team, Position), % for each fact player in the team
+    \+ member(Position, Acc), % to avoid same member
+    find_all_positions(Team, Positions, [Position|Acc]). % recursively call the find rule after adding the curr position
+find_all_positions(_, Positions, Positions). % base case when there is no other players
+
+% Calculate the frequency number for each position
+get_all_frequencies([], [], []). % the base case is when no positions
+get_all_frequencies([H|T], Unique, Counts) :- % takes all positions and returns the Unique positions and Counts of each one
     find_all_frequencies(H, T, Rest, Count),
     get_all_frequencies(Rest, RestUnique, RestCounts),
     Unique = [H|RestUnique],
     Counts = [Count|RestCounts].
 
-find_all_frequencies(_, [], [], 1).
-find_all_frequencies(H, [H|T], Rest, Count) :-
+find_all_frequencies(_, [], [], 1). % if no more positions then the count is one
+find_all_frequencies(H, [H|T], Rest, Count) :- % if H matches then increment count
     find_all_frequencies(H, T, Rest, NewCount),
     Count is NewCount + 1.
-find_all_frequencies(H, [X|T], [X|Rest], Count) :-
+find_all_frequencies(H, [X|T], [X|Rest], Count) :- % if H is not a match, skip this position
     H \= X,
     find_all_frequencies(H, T, Rest, Count).
 
 % Find the maximum frequency and its associated position
-get_max_frequency([H], [F], H, F).
+get_max_frequency([H], [F], H, F). % base case if only one element then it is the max
 get_max_frequency([H1|T1], [F1|T2], MaxPos, MaxFreq) :-
     get_max_frequency(T1, T2, TempPos, TempFreq),
-    (   F1 > TempFreq
+    (   F1 > TempFreq % compare and update if curr is higher
     ->  MaxPos = H1, MaxFreq = F1
     ;   MaxPos = TempPos, MaxFreq = TempFreq
     ).
@@ -152,4 +156,5 @@ get_max_frequency([H1|T1], [F1|T2], MaxPos, MaxFreq) :-
 most_common_position_in_team(Team, MostCommonPosition) :-
     get_all_positions(Team, Positions),
     get_all_frequencies(Positions, UniquePositions, Frequencies),
-    get_max_frequency(UniquePositions, Frequencies, MostCommonPosition, _).
+    get_max_frequency(UniquePositions, Frequencies, MostCommonPosition, _),
+    !. % cut to prevent multiple answers
